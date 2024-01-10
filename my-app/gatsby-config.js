e71d9919ccb00b7b512229006e1a978007da0baa
Vivek <vivek.bhatt@bizhues.com>
2024-01-10 12:21:00 +0530

    basic website with data fetched

--- my-app/gatsby-config.js
+++ my-app/gatsby-config.js
@@ -39,5 +39,11 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        url: `http://localhost:8000/graphql`, // Replace with your WordPress site URL
+      },
+    },
   ],
 }
